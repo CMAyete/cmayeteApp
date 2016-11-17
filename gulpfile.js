a6542aeea64e511788ea799624d8df288960c69a
Ricardo Garcia <ricardo@rjgarcia.es>
2016-11-17 12:55:00 +0100

    Compression
    App files have been minified and concatenated.

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,14 @@
 // load the plugins
 var gulp       = require('gulp');
-var minifyCSS  = require('gulp-clean-css');
-var rename     = require('gulp-rename');
-var jshint     = require('gulp-jshint');
 var concat     = require('gulp-concat');
 var uglify     = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
-var nodemon    = require('gulp-nodemon');
 
-//CSS Minifying task
-gulp.task('css', function() {
- // grab the less file, process the LESS, save to style.css
-return gulp.src('public/assets/css/style.css')
-	.pipe(minifyCSS())
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(gulp.dest('public/assets/css'));
-});
-
-// JS Linting, Concatetnation and Uglifying task
-gulp.task('scripts', function() {
-return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(concat('all.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('public/dist'));
-});
-
-// JS LCU task for Angular files
+// minify and concat angular files (cannot be done as standard js files due to angular restrictions)
 gulp.task('angular', function() {
-return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(ngAnnotate())
-	.pipe(concat('app.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('public/dist'));
+  return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+         .pipe(ngAnnotate())
+         .pipe(concat('app.js'))
+         .pipe(uglify())
+         .pipe(gulp.dest('public/dist'));
 });
-
-
-//Watch task
-gulp.task('watch', function() {
-	gulp.watch('public/assets/css/style.less', ['css']);
-	gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['js', 'angular']);
-});
-
-//Nodemon task
-gulp.task('nodemon', function() {
-	nodemon({
-		script: 'server.js',
-		ext: 'js html'
-	})
-	.on('start', ['watch'])
-	.on('change', ['watch'])
-	.on('restart', function() {
-		console.log('Restarted!');
-	});
-});
-
-//Main task
-gulp.task('default', ['nodemon']);
